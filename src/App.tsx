a07015a2e6d13ef4021b2d39992dc794f364454b
zura tetrashvili <zuratetra@gmail.com>
2025-03-27 22:07:26 +0400

    add input range

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import Buttons from "./components/Buttons";
-import Monitor from "./components/monitor";
+import Monitor from "./components/monitor/Monitor";
 
 function App() {
   return (
     <>
-      <div className="w-[320px] md:w-[540px]">
-        <Monitor />
-        <Buttons />
+      <div className="min-h-screen w-screen bg-[#5656b6] flex justify-center items-center">
+        <div className="w-[320px] md:w-[540px]">
+          <Monitor />
+          <Buttons />
+        </div>
       </div>
     </>
   );
--- src/components/monitor/Monitor.tsx
+++ src/components/monitor/Monitor.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
+import "./monitor.css";
 
 function Monitor() {
   const [calcColor, setCalcColor] = useState<number>(0);
+  console.log("🚀 ~ Monitor ~ calcColor:", calcColor);
 
   return (
     <div>
       <div className="flex justify-between items-center">
         <p className="text-white text-[32px] font-bold">calc</p>
-        <div className="flex items-center">
-          <p className="text-xs text-white">THEME</p>
+        <div className="flex items-end">
+          <p className="text-xs text-white pb-1.5">THEME</p>
           <div className="ml-6.5">
             <div className="text-white">
               <div className="flex justify-center gap-3">
@@ -23,10 +25,11 @@ function Monitor() {
                   setCalcColor(parseInt(e.target.value));
                 }}
                 type="range"
+                defaultValue={1}
                 min="1"
                 max="3"
                 // ვერ ვუცვლი ფერს მოძრავ წრეს
-                className="peer w-[71px] px-2 h-6.5 bg-blue-900  rounded-[13px] appearance-none cursor-pointer"
+                className="peer w-[71px] px-2 h-6.5 bg-blue-900 rounded-[13px] appearance-none cursor-pointer"
               />
             </div>
           </div>
